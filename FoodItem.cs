f55d60e514d21ec6915fb101b6df8328e1be3957
michajones16 <michajones16@gmail.com>
2026-01-20 19:25:35 -0700

    Menu

---
+++ FoodItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mission3
+{
+    internal class FoodItem
+    {
+        public FoodItem()
+        {
+            
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Food Bank System!");
+        Console.WriteLine("Pick a number to continue:");
+        Console.WriteLine("1: Add Food Items");
+        Console.WriteLine("2: Delete Food Items");
+        Console.WriteLine("3: Print List of Current Food Items");
+        Console.WriteLine("4: Exit the Program");
+        string userChoice = Console.ReadLine();
+
+        while (userChoice == "1" || userChoice == "2" || userChoice == "3")
+        {
+            if (userChoice == "1")
+            {
+                Console.WriteLine("Enter a name for the item: ");
+                string itemName = Console.ReadLine();
+
+                Console.WriteLine("Enter a category for the item: ");
+                string itemCategory = Console.ReadLine();
+
+                Console.WriteLine("Enter a quantity for the item:");
+
+                int itemQuantity;
+
+                try
+                {
+                    itemQuantity = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter a whole number.");
+                }
+            }
+
+            Console.WriteLine("Welcome to the Food Bank System!");
+            Console.WriteLine("Pick a number to continue:");
+            Console.WriteLine("1: Add Food Items");
+            Console.WriteLine("2: Delete Food Items");
+            Console.WriteLine("3: Print List of Current Food Items");
+            Console.WriteLine("4: Exit the Program");
+            userChoice = Console.ReadLine();
+        }
+    }
+}
