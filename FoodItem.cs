579a2808cf100b4213214f7f3b19ac03ff572d9a
michajones16 <michajones16@gmail.com>
2026-01-22 23:43:23 -0700

    added comments

--- FoodItem.cs
+++ FoodItem.cs
@@ -6,11 +6,13 @@ namespace Mission3
 {
     internal class FoodItem
     {
+        // Class attributes (get means main class can view attribute)
         public string Name { get; }
         public string Category { get; }
         public int Quantity { get; }
         public DateTime ExpDate { get; }
 
+        // FoodItem constructor
         public FoodItem(string name, string category, int quantity, DateTime expDate)
         {
             Name = name;
--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
-﻿using Mission3;
+﻿// Michael Jones - Section 1
+
+using Mission3;
 using System.Collections.Generic;
 
 internal class Program
 {
+    // Function that handles number input and related error handling
     private static int NumberInput()
     {
         while (true)
@@ -18,6 +21,7 @@ internal class Program
         }
     }
 
+    // Function that handles date/time input and related error handling
     private static int[] DateInput()
     {
         Console.WriteLine("Enter expiration year:");
@@ -46,6 +50,7 @@ internal class Program
         return [expYear, expMonth, expDay];
     }
 
+    // Prints food item lists (name - category - quantity - expDate)
     private static void printList(List<FoodItem> list)
     {
         list = list.OrderBy(item => item.ExpDate).ToList();
@@ -60,6 +65,7 @@ internal class Program
 
     private static void Main(string[] args)
     {
+        // Menu
         Console.WriteLine("Welcome to the Food Bank System!");
         Console.WriteLine("\n1: Add Food Items");
         Console.WriteLine("2: Delete Food Items");
@@ -68,10 +74,13 @@ internal class Program
         Console.WriteLine("\nPick a number to continue:");
         string userChoice = Console.ReadLine();
 
+        // List of all food items entered
         List<FoodItem> allFood = new List<FoodItem>();
 
+        // Checks to see if user has chosen not to exit
         while (userChoice != "4")
         {
+            // Add item
             if (userChoice == "1")
             {
                 Console.WriteLine("Enter a name for the item: ");
@@ -85,6 +94,7 @@ internal class Program
                 int itemQuantity = NumberInput();
                 DateTime itemExpDate;
 
+                // Checks to see if date is valid
                 while (true)
                 {
                     try
@@ -98,22 +108,34 @@ internal class Program
                         Console.WriteLine("Invalid date. Please try again.");
                     }
                 }
+                
+                // Creates new food item once successful
                 FoodItem newFood = new FoodItem(itemName, itemCategory, itemQuantity, itemExpDate);
                 allFood.Add(newFood);
             }
+            // Delete item
             else if (userChoice == "2")
             {
+                // Chosen item name
                 string itemName = "";
+
+                // List of all food items with that chosen name
                 List<FoodItem> matchingFood = new List<FoodItem>();
+
+                // List of all the indexes of the food items in the full item list
                 List<int> listIndexes = new List<int>();
 
+                // Checks to see if there are any items with the selected name
+                // (will always be 0 to start)
                 while (matchingFood.Count == 0)
                 {
                     Console.WriteLine("Enter the name of the item you would like to delete:");
                     itemName = Console.ReadLine();
 
+                    // Sorts list by expiration date (ascending)
                     allFood = allFood.OrderBy(item => item.ExpDate).ToList();
 
+                    // Loops through full item list and adds them to food list and index list
                     for (int i = 0; i < allFood.Count; i++)
                     {
                         if (allFood[i].Name == itemName)
@@ -123,6 +145,7 @@ internal class Program
                         }
                     }
 
+                    // Prints list of all food with that name and asks user which one to delete
                     if (matchingFood.Count > 1)
                     {
                         printList(matchingFood);
@@ -133,18 +156,25 @@ internal class Program
                             Console.WriteLine("Enter an number on the list.");
                             itemIndex = NumberInput();
                         }
+
+                        // Deletes food from the full item list
                         allFood.RemoveAt(listIndexes[itemIndex - 1]);
                     }
+
+                    // Deletes the item without printing if only one item was found
                     else if (matchingFood.Count == 1)
                     {
                         allFood.RemoveAt(listIndexes[0]);
                     }
+
+                    // Tells user no item was found
                     else
                     {
                         Console.WriteLine("No food item was found with that name.\n");
                     }
                 }
             }
+            // Print list of all items
             else if (userChoice == "3")
             {
                 if (allFood.Count > 0)
@@ -157,6 +187,7 @@ internal class Program
                 }
             }
 
+            // Menu (printed again until user presses 4)
             Console.WriteLine("\nWelcome to the Food Bank System!");
             Console.WriteLine("\n1: Add Food Items");
             Console.WriteLine("2: Delete Food Items");
